4810bc2aafc79fad0ac2fa3c5a005fc86080725d
Andrey Sergeevich <kaymantex68@yandex.ru>
2020-10-05 18:19:57 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import classes from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [number,setNumber]=useState(false);
+  const [number1,setNumber1]=useState(false);
+
+
+
+  return (  
+    <>
+    <div className={classes.Background}>
+      background нужно что бы здесь была очень длиная надпись , прям длянная что капец, а теперь , позвоните нам: +7 920 4897437
     </div>
+    <div className={classes.main_window}>
+      <div className={classes.div1}></div>
+      <div className={classes.div2}>
+        <div className={classes.div3}>
+          <ul>
+            <li className={classes.Link} onClick={()=>{setNumber(!number);setNumber1(number1)}}>main</li>
+            {console.log(number)}
+            <li className={classes.Link} onClick={()=>{setNumber(false); setNumber1(false)}}>monitor</li>
+            <li className={classes.Link} onClick={()=>{setNumber(number); setNumber1(!number1)}}>second</li>
+          </ul>
+        </div>
+          
+        <div className={classes.div4} >
+          <div className={(number===true) ? classes.div5 : classes.div5_2}>Просто какая то информация которая не имеет никакого значения, она нужна только для теста</div>
+          <div className={(number1===true) ? classes.div6 : classes.div6_2 }>И здесь все тоже самое, совершенно не важные вещи, так всякая фигня что бы затестить блок</div>
+        </div>
+      </div>
+    </div>
+    
+    </>
+    
   );
 }
 
