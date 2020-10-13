ae4dcf07b83f39cbedddd97b43e3f947f421312e
Home <kaymantex68@yandex.ru>
2020-10-13 18:29:37 +0300

    home content 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import classes from './App.module.css';
 import Contacts from './Contacts'
-
+import logo from './cayman.svg'
 function App() {
 
   const [first_col_main, setfirst_col_main] = useState(true);
@@ -14,17 +14,19 @@ function App() {
 
 
 
+
+
   return (
     <>
       <div className={classes.Background}>
         <Contacts />
-    </div>
+      </div>
       <div className={classes.main_window}>
         <div className={classes.div1}>
           {/* <div className={(topmenu === true) ? classes.div_top_menu : classes.div_top_menu_2}><span className={classes.SpanLogo}>top menu</span></div> */}
 
           <div className={(topmenu === true) ? classes.div_top_menu : classes.div_top_menu_2}>
-            <div className={classes.logo}><span className={classes.SpanLogo}>CAYMAN</span></div>
+            {/* <div className={classes.logo}><span className={classes.SpanLogo}>CAYMAN</span></div> */}
             <div className={classes.otstup}>отступ</div>
             <div>
               <ul className={classes.Ul_top}>
@@ -44,32 +46,68 @@ function App() {
         </div>
         <div className={classes.div2}>
           <div className={classes.div3}>
+
             <div className={(leftmenu === true) ? classes.div_left : classes.div_left_2}>
+              <div className={classes.bigLogo}>
+                <img src={logo}></img>
+              </div>
               <ul>
 
-                <li className={classes.Link} ><span className={classes.span_Link} onClick={() => { setfirst_col_main(true); setsecond_col_main(false);  setthird_col_main(true); setforth_col_main(false); setMonitoring(false); setLeftMenu(true); setTopMenu(true); }}>Главная</span></li>
+                <li className={classes.Link} ><span className={classes.span_Link} onClick={() => { setfirst_col_main(true); setsecond_col_main(false); setthird_col_main(true); setforth_col_main(false); setMonitoring(false); setLeftMenu(true); setTopMenu(true); }}>Главная</span></li>
                 <li className={classes.Link}> <span className={classes.span_Link} onClick={() => { setfirst_col_main(false); setsecond_col_main(true); setthird_col_main(false); setforth_col_main(true); setMonitoring(false); setLeftMenu(true); setTopMenu(true); }}>Модульные котельные</span></li>
                 <li className={classes.Link}> <span className={classes.span_Link} onClick={() => { setfirst_col_main(false); setsecond_col_main(true); setthird_col_main(false); setforth_col_main(true); setMonitoring(false); setLeftMenu(true); setTopMenu(true); }}>Гальваника</span></li>
-                <li className={classes.Link}><span className={classes.span_Link} onClick={() => { setfirst_col_main(false);  setsecond_col_main(false); setthird_col_main(false); setforth_col_main(false); setMonitoring(true); setLeftMenu(true); setTopMenu(true); }}>Мониторинг</span></li>
-                <li className={classes.Link}><span className={classes.span_Link} onClick={() => { setfirst_col_main(false);  setsecond_col_main(false); setthird_col_main(false); setforth_col_main(false); setMonitoring(true); setLeftMenu(true); setTopMenu(true); }}>Мониторинг ПДУ</span></li>
+                <li className={classes.Link}><span className={classes.span_Link} onClick={() => { setfirst_col_main(false); setsecond_col_main(false); setthird_col_main(false); setforth_col_main(false); setMonitoring(true); setLeftMenu(true); setTopMenu(true); }}>Мониторинг</span></li>
+                <li className={classes.Link}><span className={classes.span_Link} onClick={() => { setfirst_col_main(false); setsecond_col_main(false); setthird_col_main(false); setforth_col_main(false); setMonitoring(true); setLeftMenu(true); setTopMenu(true); }}>Мониторинг ПДУ</span></li>
                 <li className={classes.Link}><span className={classes.span_Link} onClick={() => { setfirst_col_main(false); setsecond_col_main(false); setthird_col_main(false); setforth_col_main(false); setMonitoring(false); setLeftMenu(false); setTopMenu(false); }}>Контакты</span></li>
-                <li className={classes.Link}><span className={classes.span_Link} onClick={() => { setfirst_col_main(first_col_main);  setthird_col_main(third_col_main); setthird_col_main(third_col_main); setforth_col_main(forth_col_main); setMonitoring(monitoring); setLeftMenu(false); setTopMenu(topmenu); }}>Боковое меню</span></li>
+                <li className={classes.Link}><span className={classes.span_Link} onClick={() => { setfirst_col_main(first_col_main); setthird_col_main(third_col_main); setthird_col_main(third_col_main); setforth_col_main(forth_col_main); setMonitoring(monitoring); setLeftMenu(false); setTopMenu(topmenu); }}>Боковое меню</span></li>
 
               </ul>
             </div>
           </div>
 
+
+
+
           <div className={classes.div4} >
-            <div className={(first_col_main === true) ? classes.div5 : classes.div5_2}>Просто какая то информация которая не имеет никакого значения, она нужна только для теста</div>
+            <div className={(first_col_main === true) ? classes.div5 : classes.div5_2}>
+              <p className={classes.upWord}>С какими ПЛК работаем</p>
+              <p>- Schneider Electric: modicon m221, modicon m241</p>
+              <p>- ПЛК Овен (весь модельный ряд)</p>
+              <p>- Segnetics</p>
+              <p>- WAGO</p>
+              <p>- Arduino-совместимые контроллеры</p>
+              <p>- Raspberry PI</p>
+              <p>Программируем на ST, CFC, FBD</p>
+              <p></p>
+              <p className={classes.upWord} >Среды разработки</p>
+              <p>- Codesys</p>
+              <p>- EcoStruxure Machine Expert</p>
+              <p>- Node-RED</p>
+              <p className={classes.upWord} >Вспомогательные языки программирования</p>
+              <p>- JavaScript</p>
+              <p>- Python</p>
+              <p>- C++</p>
+              <p className={classes.upWord} >SCADA системы + OPC</p>
+              <p>- Simple scada</p>
+              <p>- Овен Телематика</p>
+              <p>- Master scada</p>
+              <p>- Lectus</p>
+              <p>- OWEN OPC</p>
+
+
+
+
+
+            </div>
             <div className={(second_col_main === true) ? classes.div55 : classes.div55_2}>Просто какая то информация которая не имеет никакого значения, она нужна только для теста</div>
 
             <div className={(third_col_main === true) ? classes.div6 : classes.div6_2}>И здесь все тоже самое, совершенно не важные вещи, так всякая фигня что бы затестить блок</div>
             <div className={(forth_col_main === true) ? classes.div66 : classes.div66_2}>И здесь все тоже самое, совершенно не важные вещи, так всякая фигня что бы затестить блок</div>
 
             <div className={(monitoring === true) ? classes.div7 : classes.div7_2}>
-             <din className={classes.div8}>
-              <iframe src="http://newsiterad.teplotehnika68.ru/pic.php" width="100%" height="100%" frameborder="no" sandbox seamless>
-              </iframe>
+              <din className={classes.div8}>
+                <iframe src="http://newsiterad.teplotehnika68.ru/pic.php" width="100%" height="100%" frameborder="no" sandbox seamless>
+                </iframe>
 
               </din>
             </div>
--- src/Contacts.js
+++ src/Contacts.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import classes from './Contacts.module.css';
-
+import logo from './cayman.svg'
 function Contacts() {
     return (
         <div className={classes.Background}>
-            <div><span className={classes.Cayman}>CAYMAN </span><span className={classes.Automation}>автоматизация</span> </div>
-            <div className={classes.Phone}>
+            <div className={classes.LogoContacts}>
+                <img src={logo}></img>
+            </div>
+            <div><span className={classes.Automation}>автоматизация</span> </div>
+            <div >
+            <span className={classes.Phone}>
                 +7 (920) 4897437
+                </span>
              </div>
         </div>
     )
